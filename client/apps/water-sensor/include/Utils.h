9ec76ca9730add29d319dc7a3ea7c5c5e2091c89
gerardorodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2026-04-25 16:24:49 +0100

    Updates Utils.h

--- client/apps/water-sensor/include/Utils.h
+++ client/apps/water-sensor/include/Utils.h
@@ -7,6 +7,9 @@
 constexpr unsigned int BAUD_RATE = 9600;
 constexpr unsigned int META_DATA_FORMAT_SIZE = COMMUNICATION_TYPE_CODE_SIZE + 3 + DEVICE_TYPE_CODE_SIZE + 1;
 
+/**
+ * Moves string to `metaData` buffer in format MD:T=WL where it means MetaDataCommunicationType:T=DeviceType
+ */
 constexpr void metaData(char *metaData, const DeviceType deviceType) {
     constexpr auto format = "%s:T=%s";
     snprintf(metaData, META_DATA_FORMAT_SIZE, format, communicationTypeToString(CommunicationType::META_DATA),
@@ -17,8 +20,12 @@ constexpr unsigned int dataSize(const DeviceType deviceType) {
     return COMMUNICATION_TYPE_CODE_SIZE + 3 + deviceTypeToDataSize(deviceType) + 1;
 }
 
-constexpr void data(char *data, const DeviceType deviceType, const unsigned int value) {
+/**
+ * Moves string to `data` buffer in format DT:D=100 where it means DataCommunicationType:D=Data
+ */
+constexpr void data(char *data, const unsigned int dataSize, const unsigned int value) {
+    memset(data, 0, dataSize);
     constexpr auto format = "%s:D=%d";
-    snprintf(data, dataSize(deviceType), format, communicationTypeToString(CommunicationType::META_DATA), value);
+    snprintf(data, dataSize, format, communicationTypeToString(CommunicationType::DATA), value);
 }
 #endif //UTILS_H
