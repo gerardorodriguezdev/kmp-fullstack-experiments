c5deb2cf539d431f2c628bc39a4b312519096118
gerardorodriguez <44345493+gerardorodriguezdev@users.noreply.github.com>
2026-05-28 09:39:33 +0100

    Updates KtorKeystoreEncryptor.kt

--- server/shared/authentication/src/jvmMain/kotlin/oneclick/server/shared/authentication/security/KtorKeystoreEncryptor.kt
+++ server/shared/authentication/src/jvmMain/kotlin/oneclick/server/shared/authentication/security/KtorKeystoreEncryptor.kt
@@ -1,20 +1,18 @@
 package oneclick.server.shared.authentication.security
 
 import io.ktor.util.*
-import oneclick.shared.security.SecureRandomProvider
 import oneclick.shared.security.encryption.base.BaseEncryptor
 import java.security.Key
 import javax.crypto.spec.SecretKeySpec
 
-class KtorKeystoreEncryptor(
-    private val secretEncryptionKey: String,
-    secureRandomProvider: SecureRandomProvider
-) : BaseEncryptor(secureRandomProvider) {
-    override val transformation: String = "$ALGORITHM/CBC/PKCS5Padding"
+class KtorKeystoreEncryptor(private val secretEncryptionKey: String) : BaseEncryptor() {
+    override val transformation: String = "$ALGORITHM/$BLOCK_MODE/$PADDING"
 
     override fun secretKey(): Key = SecretKeySpec(hex(secretEncryptionKey), ALGORITHM)
 
     private companion object {
         const val ALGORITHM = "AES"
+        const val BLOCK_MODE: String = "GCM"
+        const val PADDING: String = "NoPadding"
     }
 }
